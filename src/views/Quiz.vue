92e6f4866bce20691718887266478fd75a464f08
Liv Reinhold <livreinhold@gmail.com>
2022-01-24 10:12:34 +0100

    opdated with simple fetch to trivia API

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,15 +1,26 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+fetch("https://opentdb.com/api.php?amount=10")
+.then(response => response.json())
+    .then(data => {console.log(data.results)
+    })
+    .catch(error => {
+         console.error("There was an error!", error);
+    });
+
 </script>
 
 <template>
   <h1>Quiz</h1>
+  <p>Her kommer et spørgsmål</p>
+  <button>Sandt</button><button>Falsk</button>
+  <input type="text" name="Fuldt svar" id="svar">
 </template>
 
 <style>
 #app 
 {
+  
   
 }
 </style>
