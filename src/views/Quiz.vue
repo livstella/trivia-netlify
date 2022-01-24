1755786368d0d5559ec5737003b8d757f102d111
Liv Reinhold <livreinhold@gmail.com>
2022-01-24 12:31:17 +0100

    fetches and display data.

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,26 +1,51 @@
-<script setup>
+<script >
 
+export default{
+  data(){
+    return {
+      quiz: []
+    }
+  },
+  mounted(){
 fetch("https://opentdb.com/api.php?amount=10")
-.then(response => response.json())
-    .then(data => {console.log(data.results)
+  .then(response => response.json())
+    .then(data => {this.quiz=data.results
+    console.log(this.quiz)
     })
     .catch(error => {
          console.error("There was an error!", error);
     });
+  }
+}
 
+  
+
+
+    
 </script>
 
 <template>
   <h1>Quiz</h1>
-  <p>Her kommer et spørgsmål</p>
+  <p>Her kommer et spørgsmål <br> {{quiz[0].question}}</p>
+   <ul>
+    <li v-for="item in quiz" >
+      {{item.question}}
+    </li>
+    
+  </ul>
+
   <button>Sandt</button><button>Falsk</button>
   <input type="text" name="Fuldt svar" id="svar">
+   <input
+      v-model="answer"
+      id="new-answer"
+      placeholder="type your answer here"
+    >
 </template>
 
 <style>
-#app 
-{
-  
-  
+input{
+  display: block;
+  margin: 5px;
 }
 </style>
