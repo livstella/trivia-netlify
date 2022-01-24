fe1aedb24c9162403ab7371fdff34e4537344606
Liv Reinhold <livreinhold@gmail.com>
2022-01-24 20:13:21 +0100

    Better fetch. Now with catch and conditional loading.

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,51 +1,61 @@
 <script >
+import { ref } from "vue";
+export default {
+  setup() {
+    const answer = ref(null);
+    const quiz = ref([]);
+    const error = ref(null);
 
-export default{
-  data(){
-    return {
-      quiz: []
-    }
+    const load = async () => {
+      try {
+        let data = await fetch("https://opentdb.com/api.php?amount=10");
+        if (!data.ok) {
+          throw Error("No data");
+        }
+        quiz.value = await data.json();
+        console.log(quiz.value.results[0].correct_answer);
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+
+    const onSubmit = () => {
+      if (answer.value == quiz.value.results[0].correct_answer) {
+        console.log("match");
+      } else {
+        console.log(quiz.value.results[0].correct_answer);
+      }
+    };
+
+    load();
+    return { onSubmit, answer, quiz };
   },
-  mounted(){
-fetch("https://opentdb.com/api.php?amount=10")
-  .then(response => response.json())
-    .then(data => {this.quiz=data.results
-    console.log(this.quiz)
-    })
-    .catch(error => {
-         console.error("There was an error!", error);
-    });
-  }
-}
-
-  
-
-
-    
+};
 </script>
 
 <template>
   <h1>Quiz</h1>
-  <p>Her kommer et spørgsmål <br> {{quiz[0].question}}</p>
-   <ul>
-    <li v-for="item in quiz" >
-      {{item.question}}
-    </li>
-    
-  </ul>
-
-  <button>Sandt</button><button>Falsk</button>
-  <input type="text" name="Fuldt svar" id="svar">
-   <input
-      v-model="answer"
-      id="new-answer"
-      placeholder="type your answer here"
-    >
+  <div v-if="quiz">
+    <p>Her kommer et spørgsmål</p>
+    <ul>
+      <li v-for="item in quiz.results" :key="item.question">
+        {{ item.question }}
+      </li>
+    </ul>
+  </div>
+  <button>True</button>
+  <button>False</button>
+  <button @click="onSubmit">Submit</button>
+  <input type="text" v-model="answer" placeholder="type your answer here" />
 </template>
 
 <style>
-input{
+input {
   display: block;
+  margin: 5px;
+}
+button {
   margin: 5px;
 }
 </style>
