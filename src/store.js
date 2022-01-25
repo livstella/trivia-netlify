d13ce9121b65ddc4961bb36c520dcfe01afa1f98
Damian Ravn <DamianRavn@gmail.com>
2022-01-25 16:02:24 +0100

    Feat: Startpage data is validated and set to state, so result page is ready to fetch the data.

--- src/store.js
+++ src/store.js
@@ -8,13 +8,14 @@ const store = createStore
         {
             difficulties: ['Easy', 'Medium', 'Hard'],
             catagories: [],
+            maxQuestionAmount: 0,
         },
         userData:
         {
             username: '',
             numberOfQuestions: 1,
             currentDifficulty: '',
-            currentCatagory: '',
+            currentCatagory: 0,
         }
         
         
@@ -29,6 +30,10 @@ const store = createStore
         {
             state.startPageData.catagories = payload;
         },
+        setMaxQuestion: (state, payload) =>
+        {
+            state.startPageData.maxQuestionAmount = payload;
+        },
     },
     actions:
     {
@@ -38,6 +43,18 @@ const store = createStore
             const data = await res.json();
             const catagories = data.trivia_categories;
             commit('setStartPageCatagories', catagories);
+        },
+        async fetchMaxQuestions({ commit }, categoryID)
+        {
+            const res = await fetch('https://opentdb.com/api_count.php?category=' + categoryID);
+            const data = await res.json();
+            const maxQuestionAmount = 
+            {
+                easy: data.category_question_count.total_easy_question_count,
+                medium: data.category_question_count.total_medium_question_count,
+                hard: data.category_question_count.total_hard_question_count,
+            };
+            commit('setMaxQuestion', maxQuestionAmount);
         }
     }
 });
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,6 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { ref, onMounted, computed } from 'vue';
+import { useRouter } from 'vue-router';
 import { useStore } from 'vuex';
 
 const store = useStore();
@@ -11,54 +12,110 @@ onMounted(()=>store.dispatch('fetchCatagories'));
 //Getting data from state
 const difficulties = computed(()=> store.state.startPageData.difficulties);
 const catagories = computed(()=> store.state.startPageData.catagories);
+const maxQuestionAmount = computed(()=> store.state.startPageData.maxQuestionAmount);
 
-const form = ref(null);
-//Submit function
+const router = useRouter();
+
+//Local varibles
+const username = ref('');
+const questionAmount = ref(0);
+const currentDifficulty = ref(difficulties.value[0]);
+const currentCatagory = ref(-1);
+
+
+//Submit used loosely i guess. No post or get made
 const submitForm = (e)=> 
-{console.log(e)
+{
     //Have to be able to click anywhere on sreen to submit
    if (e.target.nodeName == "FORM" || e.target.nodeName == "HTML" || e.target.nodeName == "BUTTON") 
    {
-       form.value.requestSubmit()
+       if(validateInput())
+       {
+           store.commit('setUserData', 
+           {
+               username: username.value,
+                numberOfQuestions: questionAmount.value,
+                currentDifficulty: currentDifficulty.value.toLowerCase(),
+                currentCatagory: currentCatagory.value,
+           });
+           console.log(store.state)
+       }
    }
+}
+//Validates the input, making sure that we have a usuable state
+function validateInput()
+{
+    if(questionAmount.value <= 0 )
+    {
+        questionAmount.value = 1;
+        window.alert("Question amount was less than 1, is now set to 1");
+    }
+    else if (questionAmount.value > maxQuestionAmount.value) 
+    {
+        questionAmount.value = maxQuestionAmount.value;
+        window.alert("Question amount was more than the max available, set to max");
+    }
+
+    if(!username.value)
+    {
+        window.alert("Username cannot be empty");
+        return false;
+    }
+
+    return true;
+
+}
+
+const categoriesLoaded = ()=>
+{
+    const id = catagories.value[0].id;
+    currentCatagory.value = id;
+    fetchMaxQuestions(id);
+    return true;
+} 
+const changeCategory = (e) =>
+{
+    fetchMaxQuestions(e.target.value);
+}
+const fetchMaxQuestions = (catagoryID) =>
+{
+    store.dispatch('fetchMaxQuestions', catagoryID);
 }
 </script>
 
 <template>
 <div class="start-page-wrapper" @click.stop="submitForm" v-click-outside="submitForm">
     <h1>Welcome to the Quiz Start Page!</h1>
-  <form ref="form" @submit.prevent>
+  <form @submit.prevent>
     <label for="usernameInput">Username:</label><br>
-    <input type="text" required="true" placeholder="Input Username here" id="usernameInput"><br>
+    <input v-model="username" type="text" required="true" placeholder="Input Username here" id="usernameInput"><br>
 
     <label for="numberOfQuestions">Number Of Questions:</label><br>
-    <input type="number" min="1" max="50" value="5" id="numberOfQuestions"><br>
+    <input v-model="questionAmount" type="number" min="1" max="50" id="numberOfQuestions"><br>
 
     <label for="difficultySelector">Select Difficulty:</label><br>
-    <select name="Choose Difficulty" id="difficultySelector">
+    <select v-model="currentDifficulty" name="Choose Difficulty" id="difficultySelector">
         <option v-for="difficulty in difficulties" :key="difficulty" :value="difficulty" >
             {{difficulty}}
         </option>
     </select><br>
 
     <!-- Catagories have to be fetched first, hence the if else -->
-    <div v-if="catagories.length > 0">
+     <div v-if="catagories.length > 0 && categoriesLoaded()"> <!-- this works because of the way js evaluates && statements  -->
         <label for="catagories">Select Catagory:</label><br>
-        <select name="Choose Catagory" id="catagories">
-            <option v-for="cat in catagories" :key="cat.id" :value="cat.name">
+        <select @readystatechange="changeCategory" @change="changeCategory" v-model="currentCatagory" name="Choose Catagory" id="catagories">
+            <option v-for="cat in catagories" :key="cat.id" :value="cat.id">
                 {{cat.name}}
             </option>
         </select><br>
     </div>
     <div v-else>Loading Data...</div>
     
-
-    <button>Submit</button>
+    <button>Start quiz!</button>
   </form>
 </div>
-  
-  
 </template>
+
 
 <style scoped>
 
